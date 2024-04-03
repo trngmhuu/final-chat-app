bcb8ed5b62e390541b32d283233ee8ba10fe54f3
Truong Minh Huu <minhhuu0705@gmail.com>
2024-04-04 00:15:58 +0700

    done model backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 // import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // reportWebVitals();
